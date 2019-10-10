9f90e3170167aa8cd93b88cea423ef5eba5030b7
Nick Waywood <nwaywood@au1.ibm.com>
2019-10-10 11:24:06 +1100

    stuff

--- src/Course/Monad.hs
+++ src/Course/Monad.hs
@@ -36,8 +36,8 @@ instance Monad ExactlyOne where
     (a -> ExactlyOne b)
     -> ExactlyOne a
     -> ExactlyOne b
-  (=<<) =
-    error "todo: Course.Monad (=<<)#instance ExactlyOne"
+  (=<<) f a = runExactlyOne (f <$> a)
+    
 
 -- | Binds a function on a List.
 --
@@ -48,8 +48,8 @@ instance Monad List where
     (a -> List b)
     -> List a
     -> List b
-  (=<<) =
-    error "todo: Course.Monad (=<<)#instance List"
+  (=<<) f  = flatten . (<$>) f
+    
 
 -- | Binds a function on an Optional.
 --
